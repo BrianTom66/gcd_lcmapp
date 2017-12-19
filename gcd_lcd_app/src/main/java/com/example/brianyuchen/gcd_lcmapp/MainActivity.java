7a0f6cb75c76728c585d776fafb25f879e9af10b
algsnoway <algsnoway@gmail.com>
2017-12-19 15:59:46 +0800

    add 4 numbers of GCD, LCM

--- gcd_lcd_app/src/main/java/com/example/brianyuchen/gcd_lcmapp/MainActivity.java
+++ gcd_lcd_app/src/main/java/com/example/brianyuchen/gcd_lcmapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     EditText eta;
     EditText etb;
+    EditText etc;
+    EditText etd;
 
     EditText gcd_ans1;
     EditText lcm_ans2;
@@ -27,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         return a;
     }
 
+    private static int gcd(int[] input)
+    {
+        int result = input[0];
+        for(int i=1; i<input.length; i++) {
+            result = gcd(result, input[i]);
+        }
+            return result;
+    }
+    //==============
+
     private static int lcm(int a, int b)
     {
         int lcm;
@@ -35,27 +47,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private static int lcm(int[] input)
+    {
+        int result = input[0];
+        for(int i=1; i<input.length; i++)
+        {
+            result = lcm(result, input[i]);
+        }
+            return result;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        eta = (EditText) findViewById(R.id.eT1);
-        etb = (EditText) findViewById(R.id.eT2);
+        eta = findViewById(R.id.eT1);
+        etb = findViewById(R.id.eT2);
+        etc = findViewById(R.id.eT3);
+        etd = findViewById(R.id.eT4);
 
 
-        gcd_ans1 = (EditText) findViewById(R.id.ans1);
-        lcm_ans2 = (EditText) findViewById(R.id.ans2);
+        gcd_ans1 = findViewById(R.id.ans1);
+        lcm_ans2 = findViewById(R.id.ans2);
 
-        btn1 = (Button) findViewById(R.id.btn1);
+        btn1 = findViewById(R.id.btn1);
 
         btn1.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 int a1 = Integer.parseInt(eta.getText().toString());
                 int b1 = Integer.parseInt(etb.getText().toString());
+                int c1 = Integer.parseInt(etc.getText().toString());
+                int d1 = Integer.parseInt(etd.getText().toString());
 
-                int gcd_num = gcd(a1, b1);
-                int lcm_num = lcm(a1, b1);
+                int [] number_arr = {a1, b1, c1, d1};
+                int gcd_num = gcd(number_arr);
+                int lcm_num = lcm(number_arr);
 
                 gcd_ans1.setText(gcd_num + "");
                 lcm_ans2.setText(lcm_num + "");
