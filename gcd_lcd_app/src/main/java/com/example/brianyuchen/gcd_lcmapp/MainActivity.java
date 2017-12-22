93ac895d20ef9e0923c0e09e5d5c55acbdd29556
algsnoway <algsnoway@gmail.com>
2017-12-22 15:16:52 +0800

    import Toast TextWatcher Spinner
    add fun: setRegion, addTextChangedListener. Import: Toast, TextWatcher

--- gcd_lcd_app/src/main/java/com/example/brianyuchen/gcd_lcmapp/MainActivity.java
+++ gcd_lcd_app/src/main/java/com/example/brianyuchen/gcd_lcmapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.brianyuchen.gcd_lcmapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Adapter;
@@ -10,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText eta;
@@ -17,11 +20,76 @@ public class MainActivity extends AppCompatActivity {
     EditText etc;
     EditText etd;
 
+
     EditText gcd_ans1;
     EditText lcm_ans2;
 
     Button btn1;
     static int choice;
+
+    private int MIN_MARK = 1;
+    private int MAX_MARK = 200;
+    //private void setRegion(EditText et)
+    private void setRegion( final EditText et)
+    {
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (start > 1)
+                {
+                    if (MIN_MARK != -1 && MAX_MARK != -1)
+                    {
+                        int num = Integer.parseInt(s.toString());
+                        if (num > MAX_MARK)
+                        {
+                            s = String.valueOf(MAX_MARK);
+                            et.setText(s);
+                        }
+                        else if(num < MIN_MARK)
+                            s = String.valueOf(MIN_MARK);
+                        return;
+                    }
+                }
+            }
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                if (s != null && !s.equals(""))
+                {
+                    if (MIN_MARK != -1 && MAX_MARK != -1)
+                    {
+                        int markVal = 0;
+                        try
+                        {
+                            markVal = Integer.parseInt(s.toString());
+                        }
+                        catch (NumberFormatException e)
+                        {
+                            markVal = 0;
+                        }
+                        if (markVal > MAX_MARK)
+                        {
+                            Toast.makeText(getBaseContext(), "數字不能超過200", Toast.LENGTH_SHORT).show();
+                            et.setText(String.valueOf(MAX_MARK));
+                        } else if(markVal == 0) {
+                            Toast.makeText(getBaseContext(), "數字不能為0", Toast.LENGTH_SHORT).show();
+                            et.setText(String.valueOf(MIN_MARK));
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+
+
 
     private static int gcd(int a, int b) {
         while (b > 0) {
@@ -65,6 +133,10 @@ public class MainActivity extends AppCompatActivity {
         etb = findViewById(R.id.eT2);
         etc = findViewById(R.id.eT3);
         etd = findViewById(R.id.eT4);
+        this.setRegion(eta);
+        this.setRegion(etb);
+        this.setRegion(etc);
+        this.setRegion(etd);
 
 
         gcd_ans1 = findViewById(R.id.ans1);
@@ -120,27 +192,36 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                int a1, b1, c1, d1;
+                int gcd_num=0, lcm_num=0;
 
 
-//                if(choice == 1) {
-//
-//                } else if(choice == 2) {
-//
-//                } else if(choice == 3) {
-//
-//                } else if(choice == 4) {
-//
-//                }
+                if (choice == 2) {
+                    a1 = Integer.parseInt(eta.getText().toString());
+                    b1 = Integer.parseInt(etb.getText().toString());
+                    int[] number_arr = {a1, b1};
+                    gcd_num = gcd(number_arr);
+                    lcm_num = lcm(number_arr);
 
+                } else if (choice == 3) {
+                    a1 = Integer.parseInt(eta.getText().toString());
+                    b1 = Integer.parseInt(etb.getText().toString());
+                    c1 = Integer.parseInt(etc.getText().toString());
+                    int[] number_arr = {a1, b1, c1};
+                    gcd_num = gcd(number_arr);
+                    lcm_num = lcm(number_arr);
 
-                int a1 = Integer.parseInt(eta.getText().toString());
-                int b1 = Integer.parseInt(etb.getText().toString());
-                int c1 = Integer.parseInt(etc.getText().toString());
-                int d1 = Integer.parseInt(etd.getText().toString());
+                } else if (choice == 4) {
+                    a1 = Integer.parseInt(eta.getText().toString());
+                    b1 = Integer.parseInt(etb.getText().toString());
+                    c1 = Integer.parseInt(etc.getText().toString());
+                    d1 = Integer.parseInt(etd.getText().toString());
 
-                int[] number_arr = {a1, b1, c1, d1};
-                int gcd_num = gcd(number_arr);
-                int lcm_num = lcm(number_arr);
+                    int[] number_arr = {a1, b1, c1, d1};
+                    gcd_num = gcd(number_arr);
+                    lcm_num = lcm(number_arr);
+
+                }
 
                 gcd_ans1.setText(gcd_num + "");
                 lcm_ans2.setText(lcm_num + "");
